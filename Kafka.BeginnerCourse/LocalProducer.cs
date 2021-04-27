92227cf88b3f1bda3d7f8f7dcd1b53792262b4fb
Loredana Lehadus <loredana.lehadus@mercell.com>
2021-04-27 17:05:59 +0300

    Kafka producer that pushes messages to a previous created topic on my local machine

---
+++ Kafka.BeginnerCourse/LocalProducer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Microsoft.Extensions.Logging;
+
+namespace Kafka.BeginnerCourse
+{
+    public interface ILocalProducer
+    {
+        Task Produce(List<(string key, string value)> messages);
+    }
+
+    public class LocalProducer : ILocalProducer
+    {
+        private readonly ILogger logger;
+        private readonly ProducerConfig config;
+        private readonly string topic = "first_topic";
+
+        public LocalProducer(ILogger<LocalProducer> logger)
+        {
+            this.logger = logger;
+
+            config = new ProducerConfig
+            {
+                BootstrapServers = "127.0.0.1:9092",
+                ClientId = Dns.GetHostName(),
+            };
+        }
+
+        public async Task Produce(List<(string key, string value)> messages)
+        {
+            using var producer = new ProducerBuilder<string, string>(config).Build();
+
+            foreach (var message in messages)
+            {
+                try
+                {
+                    var result = await producer.ProduceAsync(topic, new Message<string, string> { Key = message.key, Value = message.value });
+
+                    logger.LogInformation("\n Received new metadata. \n" +
+                                        "Topic: " + result.Topic + "\n" +
+                                        "Partition: " + result.Partition + "\n" +
+                                        "Offset: " + result.Offset + "\n" +
+                                        "Timestamp: " + result.Timestamp.UtcDateTime + "\n");
+                }
+                catch (Exception e)
+                {
+                    logger.LogError("Error while producing", e);
+                }
+            }
+        }
+    }
+}
---
+++ Kafka.BeginnerCourse/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Kafka.BeginnerCourse
+{
+    class Program
+    {
+        private static List<(string key, string value)> Messages => new List<(string key, string value)>
+        {
+            ("key_1", "Hi there"),
+            ("key_1", "My name is Loredana"),
+            ("key_1", "kafka is interesting so far"),
+            ("key_1", "just another message :)"),
+            ("key_1", "I like it!")
+        };
+
+        static async Task Main(string[] args)
+        {
+            using var host = CreateHostBuilder(args).Build();
+            using var serviceScope = host.Services.CreateScope();
+            var provider = serviceScope.ServiceProvider;
+
+            //create a kafka producer
+            var producer = provider.GetRequiredService<ILocalProducer>();
+
+            //produce messages to Kafka topic
+            await producer.Produce(Messages);
+
+
+            //create kafka consumer
+            //using (var consumer = new ConsumerBuilder<Ignore, string>(config)
+            //    //.SetValueDeserializer(customStringSerializerImpl)
+            //    .SetStatisticsHandler((_, json) => Console.WriteLine($"Stats: {json}"))
+            //    .SetPartitionsAssignedHandler((c, partitions) =>
+            //    {
+            //        // called on consumer group rebalance immediately before 
+            //        // the consumer starts reading from the partitions. You
+            //        // can override the start offsets, and even the set of
+            //        // partitions to consume from by (optionally) returning
+            //        // a list from this method.
+            //    })
+            //    .Build())
+            //{
+            //    //...
+            //}
+
+            //Console.WriteLine("Hello World!");
+        }
+
+        private static IHostBuilder
+            CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+                })
+                .ConfigureServices((_,
+                        services) =>
+                    services
+                        .AddSingleton<ILocalProducer, LocalProducer>());
+        }
+    }
+}
